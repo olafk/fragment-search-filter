e5b254bcb74e7e36daf8f18bfff915c246547c77
Olaf Kock <olaf.kock+github@liferay.com>
2023-07-27 16:23:42 +0200

    add results from other sites

--- src/main/java/com/liferay/fragment/olafkock/portletfilter/FragmentSearchPortletFilter.java
+++ src/main/java/com/liferay/fragment/olafkock/portletfilter/FragmentSearchPortletFilter.java
@@ -7,6 +7,9 @@ import com.liferay.fragment.model.FragmentCollection;
 import com.liferay.fragment.model.FragmentEntry;
 import com.liferay.fragment.service.FragmentCollectionLocalService;
 import com.liferay.fragment.service.FragmentEntryLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
@@ -14,6 +17,7 @@ import com.liferay.portal.kernel.util.WebKeys;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -84,7 +88,7 @@ public class FragmentSearchPortletFilter implements RenderFilter {
 			if (index >= 0) {
 				int formEndIndex = text.indexOf("</form>", index) + "</form>".length();
 				writer.write(text.substring(0, formEndIndex));
-				writer.write("\n<h2>Also found in</h2>\n" + more);
+				writer.write("\n<h2>Also found in:</h2>\n" + more);
 				writer.write(text.substring(formEndIndex));
 			} else {
 				// something in the DOM has changed since this plugin was written.
@@ -97,7 +101,7 @@ public class FragmentSearchPortletFilter implements RenderFilter {
 	}
 	
 	private String findMoreEntries(String searchTerm, RenderRequest request, String exclude) {
-		StringBuilder result = new StringBuilder("<ul>");
+		StringBuilder result = new StringBuilder();
 		ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
 		long groupId = themeDisplay.getScopeGroupId();
 		long globalId = themeDisplay.getCompanyGroupId();
@@ -120,10 +124,103 @@ public class FragmentSearchPortletFilter implements RenderFilter {
 		if(globalCollection!=null) {
 			fragmentCollections.addAll(globalCollection);
 		}
+		List<FragmentCollection> otherFragmentCollections = _fragmentCollectionLocalService.getFragmentCollections(0, 100);
+		otherFragmentCollections = new LinkedList<FragmentCollection>(otherFragmentCollections);
+		for (Iterator<FragmentCollection> iterator = otherFragmentCollections.iterator(); iterator.hasNext();) {
+			FragmentCollection fc = iterator.next();
+			if(fc.getCompanyId() != themeDisplay.getCompanyId() || fc.getGroupId() == groupId || fc.getGroupId() == globalId) {
+				iterator.remove();
+			}
+		}
 		
 		searchTerm = searchTerm.toLowerCase();
 		List<FragmentCollectionContributor> contributors = _fragmentCollectionContributorRegistry.getFragmentCollectionContributors();
+		
+		String part = fragmentCollectionToLI(searchTerm, exclude, themeDisplay, groupId, globalId, uri, queryString, fragmentCollections);
+		if(part.length()>8) {
+			result.append("<h3>Fragment Collections <i>in this site</i></h3><ul>")
+			.append(part)
+			.append("</ul>");
+		}
+		
+		part = contributorsToLI(searchTerm, exclude, uri, queryString, contributors);
+		if(part.length()>8) {
+			result.append("<h3>Fragment Collections Contributors <i>(not editable)</i></h3><ul>")
+			.append(part)
+			.append("</ul>");
+		}
 
+		part = fragmentCollectionToLI(searchTerm, exclude, themeDisplay, groupId, globalId, uri, queryString, otherFragmentCollections);
+		if(part.length()>8) {
+			result.append("<div style=\"opacity:50%;\"><h3>Fragment Collections <i>in other sites</i></h3>")
+			.append("<p>Note: When following any of those links, you'll show the other site's fragments in the context of this site. Don't be confused!</p>")
+			.append("<ul>")
+			.append(part)
+			.append("</ul></div>");
+		}
+		
+		return result.toString();
+	}
+
+	/***
+	 * Convert FragmentContributors to a <li></li> String, linking only the fragment contributor's name
+	 * @param searchTerm
+	 * @param exclude
+	 * @param uri
+	 * @param queryString
+	 * @param contributors
+	 * @return
+	 */
+	
+	private String contributorsToLI(String searchTerm, String exclude, String uri,
+			String queryString, List<FragmentCollectionContributor> contributors) {
+		StringBuilder result = new StringBuilder();
+		for (FragmentCollectionContributor contributor : contributors) {
+			if(contributor.getFragmentCollectionKey().equals(exclude)) {
+				continue;
+			}
+			List<FragmentEntry> fragmentEntries = contributor.getFragmentEntries();
+			for (FragmentEntry fragmentEntry : fragmentEntries) {
+				if(fragmentEntry.getName().toLowerCase().indexOf(searchTerm)>=0) {
+					result.append("<li><i>")
+					.append(fragmentEntry.getName())
+					.append("</i> from ")
+					.append("<a href=\"")
+					.append(uri)
+					.append("/fragments/fragment_collection/")
+					.append(contributor.getFragmentCollectionKey())
+					.append(queryString)
+					.append("\">")
+					.append(contributor.getName())
+					.append("</a></li>");
+				}
+			}
+		}
+		return result.toString();
+	}
+
+	/***
+	 * Convert FragmentCollections to a <li></li> String, while linking
+	 * the fragment and the fragmentCollection to proper UI URLs
+	 * 
+	 * If this collection is not in the current or global site, also 
+	 * include the site name
+	 * 
+	 * @param searchTerm
+	 * @param exclude
+	 * @param themeDisplay
+	 * @param groupId
+	 * @param globalId
+	 * @param uri
+	 * @param queryString
+	 * @param fragmentCollections
+	 * @return
+	 */
+	
+	private String fragmentCollectionToLI(String searchTerm, String exclude,
+			ThemeDisplay themeDisplay, long groupId, long globalId, String uri, String queryString,
+			List<FragmentCollection> fragmentCollections) {
+		StringBuilder result = new StringBuilder();
 		for (FragmentCollection collection : fragmentCollections) {
 			if((""+collection.getPrimaryKey()).equals(exclude)) {
 				continue;
@@ -149,32 +246,30 @@ public class FragmentSearchPortletFilter implements RenderFilter {
 					.append(queryString)
 					.append("\">")
 					.append(collection.getName())
-					.append("</a></li>");
+					.append("</a>");
+					if(fragmentEntry.getGroupId() != groupId && fragmentEntry.getGroupId() != globalId ) {
+						Group group;
+						try {
+							group = _groupLocalService.getGroup(fragmentEntry.getGroupId());
+							if(group != null) {
+								result.append(" <i>in ")
+								.append(group.getName(themeDisplay.getLocale()))
+								.append("</i>");
+							} else {
+								result.append(" <i>somewhere</i>");
+							}
+						} catch (PortalException e) {
+							result.append("... but: ")
+							.append(e.getClass().getName())
+							.append(" ")
+							.append(e.getMessage());
+						}
+					}
+
+					result.append("</li>");
 				}
 			}
 		}
-		for (FragmentCollectionContributor contributor : contributors) {
-			if(contributor.getFragmentCollectionKey().equals(exclude)) {
-				continue;
-			}
-			List<FragmentEntry> fragmentEntries = contributor.getFragmentEntries();
-			for (FragmentEntry fragmentEntry : fragmentEntries) {
-				if(fragmentEntry.getName().toLowerCase().indexOf(searchTerm)>=0) {
-					result.append("<li>")
-					.append(fragmentEntry.getName())
-					.append(" from ")
-					.append("<a href=\"")
-					.append(uri)
-					.append("/fragments/fragment_collection/")
-					.append(contributor.getFragmentCollectionKey())
-					.append(queryString)
-					.append("\">")
-					.append(contributor.getName())
-					.append("</a></li>");
-				}
-			}
-		}
-		result.append("</ul>");
 		return result.toString();
 	}
 	
@@ -188,4 +283,7 @@ public class FragmentSearchPortletFilter implements RenderFilter {
 	@Reference
 	private FragmentCollectionLocalService 
 		_fragmentCollectionLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
 }
